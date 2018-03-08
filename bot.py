2a206d64e24cfa2ce85e39aea2933dba9a1976fa
gustavofcma <gustavofcma@gmail.com>
2018-03-08 17:55:21 -0300

    testando user data and photo

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ def entrada(bot, update, args, job_queue, chat_data):
 
     update.message.reply_text('Bom dia, anotei que você chegou {}, {} {}'.format(msg_hr,fn,ln))
     update.message.reply_text('{} - {}'.format(tst1,tst2))
-    update.message.reply_photo(pf)
+    update.message.reply_photo(pf[0])
 
 
 def unset(bot, update, chat_data):
